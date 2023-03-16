cde1bd7b042626b58184795094aee57dfc4f4e03
Alexander Thougaard Domino <202108571@post.au.dk>
2023-03-16 13:32:56 +0100

    Fixed some Layoutstuff and First Day of calender = right day of week in grid

--- WeightWizard_test/Model/CalenderModel.cs
+++ WeightWizard_test/Model/CalenderModel.cs
@@ -1,6 +1,8 @@
+using WeightWizard_test.Model.Interfaces;
+
 namespace WeightWizard_test.Model;
 
-public class CalenderModel
+public class CalenderModel : ICalenderItems
 {
     public DateTime Date { get; set; }
 }
--- WeightWizard_test/ViewModel/JournalPageViewModel.cs
+++ WeightWizard_test/ViewModel/JournalPageViewModel.cs
@@ -17,7 +17,20 @@ public partial class JournalPageViewModel
     private void BindDates(DateTime selectedDate)
     {
         int daysCount = DateTime.DaysInMonth(selectedDate.Year, selectedDate.Month);
+        var firstDayOfMonth = new DateTime(selectedDate.Year, selectedDate.Month, 1);
 
+        if (firstDayOfMonth.DayOfWeek != DayOfWeek.Monday)
+        {
+            int daysBeforeMonth = (int)firstDayOfMonth.DayOfWeek - 1;
+            for (int spoofDay = 0; spoofDay < daysBeforeMonth; spoofDay++)
+            {
+                Dates.Add(new CalenderModel
+                {
+                    Date = new DateTime(selectedDate.Year, selectedDate.Month, 30)
+                });
+            }
+        }
+        
         for (int day = 1; day < daysCount; day++)
         {
             Dates.Add(new CalenderModel
