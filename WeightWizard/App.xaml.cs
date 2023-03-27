a5bbe4d57fc963c0cada3f3f8fba68b251b6a010
Alexander Thougaard Domino <202108571@post.au.dk>
2023-03-27 16:37:10 +0200

    Added Login page

--- WeightWizard/App.xaml.cs
+++ WeightWizard/App.xaml.cs
@@ -1,4 +1,6 @@
-﻿namespace WeightWizard;
+﻿using WeightWizard.View;
+
+namespace WeightWizard;
 
 public partial class App : Application
 {
@@ -6,6 +8,7 @@ public partial class App : Application
     {
         InitializeComponent();
 
-        MainPage = new AppShell();
+        MainPage = new LoginPage();
+        //MainPage = new AppShell();
     }
 }
