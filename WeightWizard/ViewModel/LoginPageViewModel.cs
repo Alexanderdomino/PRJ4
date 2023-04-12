65f9b6439eb87c1ebf5f536e70b0c8b7e6330ae0
Alexander Thougaard Domino <202108571@post.au.dk>
2023-04-12 11:12:38 +0200

    Implemented RESTful API for Login viewmodel, but need the correct server hostname

--- WeightWizard/ViewModel/LoginPageViewModel.cs
+++ WeightWizard/ViewModel/LoginPageViewModel.cs
@@ -1,20 +1,62 @@
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
+using Newtonsoft.Json;
+using System.Text;
 
-namespace WeightWizard.ViewModel;
 
-public partial class LoginPageViewModel : ObservableObject
+namespace WeightWizard.ViewModel
 {
-    [ObservableProperty] private string _username;
-    [ObservableProperty] private string _password;
-    
-    [RelayCommand]
-    private async void SignIn()
+    public partial class LoginPageViewModel : ObservableObject
     {
-        if (Username == null || Password == null)
+        [ObservableProperty] private string _username;
+        [ObservableProperty] private string _password;
+
+        private readonly HttpClient _httpClient = new HttpClient();
+
+        [RelayCommand]
+        private async void SignIn()
         {
-            Console.WriteLine("no username or password");
-        }   
-        else await Shell.Current.GoToAsync("///main");
+            if (string.IsNullOrEmpty(Username) || string.IsNullOrEmpty(Password))
+            {
+                Console.WriteLine("No username or password");
+            }
+            else
+            {
+                try
+                {
+                    var loginSuccessful = await LoginAsync(Username, Password);
+
+                    if (loginSuccessful)
+                    {
+                        await Shell.Current.GoToAsync("///main");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid username or password");
+                    }
+                }
+                catch (HttpRequestException ex)
+                {
+                    Console.WriteLine($"Error connecting to server: {ex.Message}");
+                }
+            }
+        }
+
+        private async Task<bool> LoginAsync(string email, string password)
+        {
+            var loginData = new Dictionary<string, string>
+            {
+                { "email", email },
+                { "password", password }
+            };
+
+            var jsonLoginData = JsonConvert.SerializeObject(loginData);
+
+            var requestContent = new StringContent(jsonLoginData, Encoding.UTF8, "application/json");
+
+            var response = await _httpClient.PostAsync("https://your-backend-server.com/api/login", requestContent);
+
+            return response.IsSuccessStatusCode;
+        }
     }
 }
