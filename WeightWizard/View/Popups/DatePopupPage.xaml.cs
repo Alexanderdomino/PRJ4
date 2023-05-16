fedd96fb1f767c2ae2ef9ee2a12b4a778f4be0fb
Alexander Thougaard Domino <202108571@post.au.dk>
2023-05-16 11:32:28 +0200

    Used Typecast to change selected Item in Collection to interface

--- WeightWizard/View/Popups/DatePopupPage.xaml.cs
+++ WeightWizard/View/Popups/DatePopupPage.xaml.cs
@@ -4,13 +4,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using WeightWizard.Model;
+using WeightWizard.Model.Interfaces;
 using WeightWizard.ViewModel.PopupViewmodel;
 
 namespace WeightWizard.View.Popups;
 
 public partial class DatePopupPage
 {
-    public DatePopupPage(CalenderModel selectedItem)
+    public DatePopupPage(ICalenderItems selectedItem)
     {
         InitializeComponent();
         BindingContext = new DatePopupViewModel(selectedItem);
