8b98742c187ea47ce872e3a4f07c12f4faba81a7
Alexander Thougaard Domino <202108571@post.au.dk>
2023-05-17 11:34:36 +0200

    removed unused imports

--- WeightWizard/ViewModel/LoginPageViewModel.cs
+++ WeightWizard/ViewModel/LoginPageViewModel.cs
@@ -2,7 +2,6 @@ using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 using Newtonsoft.Json;
 using System.Text;
-using Syncfusion.Maui.Core.Converters;
 using WeightWizard.Model.DTOs;
 
 
