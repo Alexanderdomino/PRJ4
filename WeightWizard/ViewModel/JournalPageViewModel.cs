4dce9e8bd6fa61ebe1a3c8e8ec1e4948122f4b8a
Alexander Thougaard Domino <202108571@post.au.dk>
2023-05-20 11:30:51 +0200

    JWT header in LoggerPageViewModel.cs

--- WeightWizard/ViewModel/JournalPageViewModel.cs
+++ WeightWizard/ViewModel/JournalPageViewModel.cs
@@ -26,7 +26,7 @@ namespace WeightWizard.ViewModel
         private bool _isLoading = false;
 
         //HttpClient for getting daily data
-        private readonly HttpClient _httpClient = new HttpClient();
+        private readonly HttpClient _httpClient = new();
         private readonly string _token = await SecureStorage.GetAsync("jwt_token");
         
         partial void OnSelectedMonthChanged(DateTime SelectedMonth)
@@ -52,6 +52,7 @@ namespace WeightWizard.ViewModel
             _isLoading = true;
             
             _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("bearer", _token);
+            
             // Check if there is a successful connection to the server
             var isConnected = await CheckServerConnectionAsync();
             if (!isConnected)
