0aaeb336abdc8fe3fd361a99690b818252041179
Alexander Thougaard Domino <202108571@post.au.dk>
2023-05-22 10:32:42 +0200

    Implemented viewmodel for RegisterUserPage.xaml to post to the backend

--- WeightWizard/Model/DTOs/UserDto.cs
+++ WeightWizard/Model/DTOs/UserDto.cs
@@ -2,8 +2,6 @@ namespace WeightWizard.Model.DTOs;
 
 public class UserDto
 {
-    public int UserId { get; set; }
-
     public string Username { get; set; } = null!;
 
     public string PasswordHash { get; set; } = null!;
