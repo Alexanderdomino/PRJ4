ed9dd9c496f257f4e741e29a7e8deb2e800016c2
Alexander Thougaard Domino <202108571@post.au.dk>
2023-05-23 14:54:35 +0200

    Added unit testing project and modified csporj for testing - This means that the app doesn't work in this branch

---
+++ WeightWizard.Test.Unit/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace WeightWizard.Test.Unit;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
